84545540a2e839fc3e58ae051edde6db2d3c425c
Shaun Canavan <scanavan@usf.edu>
2018-01-20 10:19:47 -0500

    Added comments

--- LandmarkFeatures/MatToBnd.m
+++ LandmarkFeatures/MatToBnd.m
@@ -3,10 +3,15 @@ function MatToBnd(d)
     directory = dir(d);
     index=1;
     for i = 4:size(directory,1)
+        %get file name and print to screen for updating while running
         filePath = strcat(d, '/', directory(i).name)
+        %load mat file
         matFile = load(filePath);
+        %each instance in task
         for j = 1:size(matFile.stereo, 2)
+            %gets 3D landmarks
             points = matFile.stereo(j).pts_3d;
+            %create output file based on index
             outFile = filePath(1:end-4);
             if index < 10
                 outFile = strcat(outFile,'_000',num2str(index));
@@ -18,9 +23,11 @@ function MatToBnd(d)
                 outFile = strcat(outFile, '_', num2str(index));
             end
             outFile = strcat(outFile, '.bndplus');
+            %write output file
             dlmwrite(outFile, points);
             index = index+1;
         end
+        %reset index as we are on next task
         index = 1;
     end
 end
